34cd520a906e4c1478422e45552fde07e9161dff
hopun <hopun.c@gmail.com>
2017-02-22 18:38:08 +0800

    '1-commit'

---
+++ app/src/main/java/com/example/hopunchen/sprintapplication/application/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.example.hopunchen.sprintapplication.application;
+
+import android.app.Application;
+
+/*
+ *  项目名：  SprintApplication
+ *  包名：    com.example.hopunchen.sprintapplication.application
+ *  文件名:   BaseApplication
+ *  创建者:   CHB
+ *  创建时间:  2017/2/20 21:51
+ *  描述：    Application
+ */
+public class BaseApplication extends Application {
+
+}
---
+++ app/src/main/java/com/example/hopunchen/sprintapplication/ui/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.example.hopunchen.sprintapplication.ui;
+
+/*
+ *  项目名：  SprintApplication
+ *  包名：    com.example.hopunchen.sprintapplication.ui
+ *  文件名:   BaseActivity
+ *  创建者:   CHB
+ *  创建时间:  2017/2/20 21:56
+ *  描述：    Activity基类
+ */
+public class BaseActivity {
+}
