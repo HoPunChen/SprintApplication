47e177fb6871600d67c0389a2b0370b2c78ad8c8
hopun <hopun.c@gmail.com>
2017-02-26 23:25:56 +0800

    4-commit

---
+++ app/src/main/java/com/example/hopunchen/sprintapplication/ui/SplashActivity.java
@@ -0,0 +1,91 @@
+package com.example.hopunchen.sprintapplication.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.hopunchen.sprintapplication.MainActivity;
+import com.example.hopunchen.sprintapplication.R;
+import com.example.hopunchen.sprintapplication.utils.ShareUtils;
+import com.example.hopunchen.sprintapplication.utils.StaticClass;
+import com.example.hopunchen.sprintapplication.utils.UtilTools;
+
+/**
+ * 项目名：  SprintApplication
+ * 包名：    com.example.hopunchen.sprintapplication.ui
+ * 文件名:   SplashActivity
+ * 创建者:   CHB
+ * 创建时间:  2017/2/26 20:02
+ * 描述：    闪屏页
+ **/
+public class SplashActivity extends AppCompatActivity {
+
+    /**
+     * 1.延时2000ms
+     * 2.判断程序是否第一次运行
+     * 3.自定义字体
+     * 4.Activity全屏主题
+     */
+
+    private TextView tv_splash;
+
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case StaticClass.HANDLER_SPLASH:
+                    //判断程序是否是第一次运行
+                    if (isFirst()) {
+                        startActivity(new Intent(SplashActivity.this, GuideActivity.class));
+                    } else {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    }
+                    finish();
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        initView();
+    }
+    //初始化View
+    private void initView() {
+        //延时2000ms
+        handler.sendEmptyMessageDelayed(StaticClass.HANDLER_SPLASH, 2000);
+
+        tv_splash = (TextView) findViewById(R.id.tv_splash);
+        //设置字体
+        UtilTools.setFont(this,tv_splash);
+    }
+
+    //判断程序是否第一次运行
+    private boolean isFirst() {
+        boolean isFirst = ShareUtils.getBoolean(this,StaticClass.SHARE_IS_FIRST,true);
+        if(isFirst){
+            ShareUtils.putBoolean(this,StaticClass.SHARE_IS_FIRST,false);
+            //是第一次运行
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+    //禁止返回键
+    @Override
+    public void onBackPressed(){
+        //super.onBackPressed();
+    }
+
+}
+
