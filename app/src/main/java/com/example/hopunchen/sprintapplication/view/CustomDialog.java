407df0bb6805c9ea5d19edc56b59614dd7019aff
hopun <hopun.c@gmail.com>
2017-03-02 17:52:58 +0800

    8-commit

---
+++ app/src/main/java/com/example/hopunchen/sprintapplication/view/CustomDialog.java
@@ -0,0 +1,12 @@
+package com.example.hopunchen.sprintapplication.view;
+
+/**
+ * 项目名：  SprintApplication
+ * 包名：    com.example.hopunchen.sprintapplication.view
+ * 文件名:   CustomDialog
+ * 创建者:   CHB
+ * 创建时间:  2017/3/2 17:35
+ * 描述：    TODO
+ **/
+public class CustomDialog {
+}
