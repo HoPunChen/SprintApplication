a072e3e9e63be80404e25aa3b2d4145a62005e61
hopun <hopun.c@gmail.com>
2017-03-04 00:01:35 +0800

    updata

--- app/src/main/java/com/example/hopunchen/sprintapplication/entity/MyUser.java
+++ app/src/main/java/com/example/hopunchen/sprintapplication/entity/MyUser.java
@@ -15,12 +15,24 @@ public class MyUser extends BmobUser {
     private boolean sex;
     private String desc;
 
+    public int getAge() {
+        return age;
+    }
+
     public void setAge(int age) {
         this.age = age;
     }
 
+    public boolean isSex() {
+        return sex;
+    }
+
     public void setSex(boolean sex) {
         this.sex = sex;
+    }
+
+    public String getDesc() {
+        return desc;
     }
 
     public void setDesc(String desc) {
--- app/src/main/java/com/example/hopunchen/sprintapplication/fragment/UserFragment.java
+++ app/src/main/java/com/example/hopunchen/sprintapplication/fragment/UserFragment.java
@@ -1,12 +1,24 @@
 package com.example.hopunchen.sprintapplication.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hopunchen.sprintapplication.R;
+import com.example.hopunchen.sprintapplication.entity.MyUser;
+import com.example.hopunchen.sprintapplication.ui.LoginActivity;
+
+import cn.bmob.v3.BmobUser;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
 
 /**
  * 项目名：  SmartButler
@@ -16,11 +28,118 @@ import com.example.hopunchen.sprintapplication.R;
  * 创建时间:  2017/2/20 23:09
  * 描述：    个人中心
  **/
-public class UserFragment extends Fragment{
+public class UserFragment extends Fragment implements View.OnClickListener{
+
+    private Button btn_exit_user;
+    private TextView edit_user;
+
+    private EditText et_username;
+    private EditText et_sex;
+    private EditText et_age;
+    private EditText et_desc;
+    //更新按钮
+    private Button btn_update_ok;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_user, null);
+        findView(view);
         return view;
+    }
+
+    //初始化View
+    private void findView(View view) {
+        btn_exit_user = (Button) view.findViewById(R.id.btn_exit_user);
+        btn_exit_user.setOnClickListener(this);
+        edit_user = (TextView) view.findViewById(R.id.edit_user);
+        edit_user.setOnClickListener(this);
+
+        et_username = (EditText) view.findViewById(R.id.et_username);
+        et_sex = (EditText) view.findViewById(R.id.et_sex);
+        et_age = (EditText) view.findViewById(R.id.et_age);
+        et_desc = (EditText) view.findViewById(R.id.et_desc);
+
+        btn_update_ok = (Button) view.findViewById(R.id.btn_update_ok);
+        btn_update_ok.setOnClickListener(this);
+
+        //默认是不可点击的/不可输入
+        setEnabled(false);
+
+        //设置具体的值
+        MyUser userInfo = BmobUser.getCurrentUser(MyUser.class);
+        et_username.setText(userInfo.getUsername());
+        et_age.setText(userInfo.getAge() + "");
+        et_sex.setText(userInfo.isSex() ? getString(R.string.text_boy) : getString(R.string.text_girl_f));
+        et_desc.setText(userInfo.getDesc());
+    }
+
+    //控制焦点
+    private void setEnabled(boolean is) {
+        et_username.setEnabled(is);
+        et_sex.setEnabled(is);
+        et_age.setEnabled(is);
+        et_desc.setEnabled(is);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //退出登录
+            case R.id.btn_exit_user:
+                //清除缓存用户对象
+                MyUser.logOut();
+                // 现在的currentUser是null了
+                BmobUser currentUser = MyUser.getCurrentUser();
+                startActivity(new Intent(getActivity(), LoginActivity.class));
+                getActivity().finish();
+                break;
+            //编辑资料
+            case R.id.edit_user:
+                setEnabled(true);
+                btn_update_ok.setVisibility(View.VISIBLE);
+            case R.id.btn_update_ok:
+                //1.拿到输入框的值
+                String username = et_username.getText().toString();
+                String age = et_age.getText().toString();
+                String sex = et_sex.getText().toString();
+                String desc = et_desc.getText().toString();
+                //2.判断是否为空
+                if (!TextUtils.isEmpty(username) & !TextUtils.isEmpty(age) & !TextUtils.isEmpty(sex)) {
+                    //3.更新属性
+                    MyUser user = new MyUser();
+                    user.setUsername(username);
+                    user.setAge(Integer.parseInt(age));
+                    //性别
+                    if (sex.equals(getString(R.string.text_boy))) {
+                        user.setSex(true);
+                    } else {
+                        user.setSex(false);
+                    }
+                    //简介
+                    if (!TextUtils.isEmpty(desc)) {
+                        user.setDesc(desc);
+                    } else {
+                        user.setDesc(getString(R.string.text_nothing));
+                    }
+                    BmobUser bmobUser = BmobUser.getCurrentUser();
+                    user.update(bmobUser.getObjectId(), new UpdateListener() {
+                        @Override
+                        public void done(BmobException e) {
+                            if (e == null) {
+                                //修改成功
+                                setEnabled(false);
+                                btn_update_ok.setVisibility(View.GONE);
+                                Toast.makeText(getActivity(), R.string.text_editor_success, Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getActivity(), R.string.text_editor_failure, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.text_tost_empty), Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
     }
 }
